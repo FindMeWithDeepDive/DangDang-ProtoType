097da716035e81a312a75c55ec10016e0dd0b6e5
LimJaeHyun <guswogus81@gmail.com>
2025-02-14 23:07:23 +0900

    🌟 Feat : 위치 페이지 생성

--- src/pages/place-page/PlacePage.tsx
+++ src/pages/place-page/PlacePage.tsx
@@ -1,3 +1,27 @@
+import styled from "styled-components";
+
 export default function PlacePage() {
-  return <div>장소 페이지입니다</div>;
+  return (
+    <PlacePageContainer>
+      <Header>헤더입니당 (수정예정)</Header>
+      <Map></Map>
+    </PlacePageContainer>
+  );
 }
+
+const Header = styled.div`
+  width: 440px;
+  height: 51px;
+  background-color: #d9d9d9;
+  text-align: center;
+`;
+
+const PlacePageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #d1d1d1;
+`;
+
+const Map = styled.div`
+  width: 440px;
+`;
