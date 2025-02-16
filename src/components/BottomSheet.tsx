d3561ac90499ce0451a0d58198eed71af4632dc1
LimJaeHyun <guswogus81@gmail.com>
2025-02-16 22:48:02 +0900

    📝 Update : 바텀 시트 컴포넌트 마크업 완료

--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -1,115 +1,86 @@
-import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import bottomBarIcon from "../assets/bottomBarIcon.png";
-import starIcon from "../assets/starIcon.png";
-import phoneIcon from "../assets/phoneIcon.png";
+import BottomSheetContent from "./BottomSheetContent";
+import BottomSheetMeetingList from "./BottomSheetMeetingList";
 
 export default function BottomSheet() {
+  const [height, setHeight] = useState(28); // 기본 height 값을 400으로 설정
+  const [contentDisplay, setContentDisplay] = useState("none");
+  const [meetingDisplay, setMeetingDisplay] = useState("none");
+  const [state, setState] = useState(0);
+
+  const onClickHeader = () => {
+    if (state === 0) {
+      setState(1);
+      setHeight(195);
+      setContentDisplay("flex");
+      setMeetingDisplay("flex");
+    } else if (state === 1) {
+      setState(2);
+      setHeight(900);
+    } else if (state === 2) {
+      setState(0);
+      setHeight(28);
+      setContentDisplay("none");
+      setMeetingDisplay("none");
+    }
+  };
+
   return (
-    <BottomSheetContainer>
-      <BottomHeader>
+    <BottomSheetContainer height={height}>
+      <BottomHeader
+        onClick={() => {
+          onClickHeader();
+        }}
+      >
         <BottomBarIcon src={bottomBarIcon} />
       </BottomHeader>
-      <SheetContentWrapper>
-        <SheetContent>
-          <TitleIconWrapper>
-            <Title>제목</Title>
-            <Icon src={starIcon} />
-          </TitleIconWrapper>
-          <PlaceDescription>부산 해운대구 어디어디</PlaceDescription>
-          <PlaceDescriptionSub>(구) 부산 해운대구 어디어디</PlaceDescriptionSub>
-          <PhoneNumberWrapper>
-            <PhoneIcon src={phoneIcon} />
-            <PhoneNumber>053-111-2345</PhoneNumber>
-          </PhoneNumberWrapper>
-        </SheetContent>
+      <SheetContentWrapper contentDisplay={contentDisplay}>
+        <BottomSheetContent />
       </SheetContentWrapper>
+      <SheetMeetingListWrapper meetingDisplay={meetingDisplay}>
+        <BottomSheetMeetingList />
+      </SheetMeetingListWrapper>
     </BottomSheetContainer>
   );
 }
 
+// BottomSheetContainer의 height를 props로 받음
 const BottomSheetContainer = styled.div`
   width: 440px;
   background-color: #ffffff;
   position: absolute;
   z-index: 1;
   display: flex;
-  bottom: 6px;
+  bottom: 65px;
   flex-direction: column;
+  height: ${({ height }) => height}px; /* height를 props로 받음 */
+  border-top-left-radius: 20px; /* Top-left corner rounded */
+  border-top-right-radius: 20px; /* Top-right corner rounded */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Subtle shadow */
+  transition: height 0.3s ease-in-out; /* height가 변경될 때 부드럽게 애니메이션 */
 `;
 
 const BottomHeader = styled.div`
   height: 36px;
   margin: 0 auto;
+  margin-top: 8px;
+  cursor: pointer;
 `;
 
 const BottomBarIcon = styled.img`
   justify-content: center;
+  margin: 0 auto;
 `;
 
 const SheetContentWrapper = styled.div`
   width: 440px;
-  margin-bottom: 32px;
+  display: ${({ contentDisplay }) => contentDisplay};
 `;
 
-const SheetContent = styled.div`
-  width: 440px;
-`;
-
-const TitleIconWrapper = styled.div`
-  width: 440px;
-  height: 32px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
+const SheetMeetingListWrapper = styled.div`
+  margin: 0 16px;
   margin-bottom: 16px;
-`;
-
-const Title = styled.div`
-  width: 200px;
-  color: #845ec2;
-  font-size: bold;
-  font-size: 20px;
-  margin-left: 16px;
-`;
-
-const Icon = styled.img`
-  width: 25px;
-  height: 25px;
-  margin-left: auto;
-  margin-right: 16px;
-`;
-
-const PlaceDescription = styled.div`
-  width: 400px;
-  height: 20px;
-  font-size: 15px;
-  margin-left: 16px;
-  margin-bottom: 8px;
-`;
-
-const PlaceDescriptionSub = styled.div`
-  width: 400px;
-  height: 16px;
-  font-size: 13px;
-  margin-left: 16px;
-  margin-bottom: 24px;
-`;
-
-const PhoneNumberWrapper = styled.div`
-  width: 440px;
-  height: 24px;
-  display: flex;
-  margin-left: 16px;
-`;
-
-const PhoneIcon = styled.img`
-  width: 16px;
-  height: 16px;
-  margin-right: 8px;
-`;
-const PhoneNumber = styled.div`
-  width: 200px;
-  height: 21px;
-  font-size: 15px;
+  display: ${({ meetingDisplay }) => meetingDisplay};
 `;
