98826747103defd25fda9f383cc3897e700749c4
LimJaeHyun <guswogus81@gmail.com>
2025-02-19 21:30:52 +0900

    🌟 Feat : stomp 기본 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,20 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: "globalThis",
+  },
   server: {
     proxy: {
       "/api": {
-        target: "http://54.180.30.146:8080", // 백엔드 서버 주소
+        target: "http://54.180.30.146:8080",
+        changeOrigin: true,
+        secure: false,
+      },
+      "/ws": {
+        // WebSocket 프록시 추가
+        target: "ws://54.180.30.146:8080",
+        ws: true,
         changeOrigin: true,
         secure: false,
       },
